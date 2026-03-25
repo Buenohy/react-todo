907719b7aa6e84bf9102af1e6f45de61208c13ce
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-03-25 15:17:41 -0300

    feat: created InputText component

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,11 @@ import Icon from './components/icon';
 import Badge from './components/badge';
 import Button from './components/button';
 import ButtonIcon from './components/button-icon';
+import InputText from './components/input-text';
 
 export default function App() {
   return (
-    <div className="grid gap-3">
+    <div className="grid gap-10">
       <div className="flex flex-col gap-2">
         <Text variant="body-sm-bold" className="text-pink-base">
           Olá mundo!
@@ -43,6 +44,10 @@ export default function App() {
         <ButtonIcon icon={TrashIcon} />
         <ButtonIcon icon={TrashIcon} variant="secondary" />
         <ButtonIcon icon={TrashIcon} variant="tertiary" />
+      </div>
+
+      <div>
+        <InputText />
       </div>
     </div>
   );
